c694abc274142070148ac0da142b7dad06b80528
tamnguyen2002 <95187098+tamnguyen2002@users.noreply.github.com>
2021-11-29 12:20:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Trần Hoa',
+    introTitle: 'Trần Thị thanh Tuyền',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
